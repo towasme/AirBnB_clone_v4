ce9c821a014b468ee5aedc9cbe47739ffef7100d
towasme <towasm@gmail.com>
2020-05-16 13:21:36 -0500

    added status to work with the api

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -14,3 +14,13 @@ $(document).ready(function () {
     }
   });
 });
+
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+  if (status === 'success') {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  }
+});
