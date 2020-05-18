7c2820ba343e7bc4005170db3bfc48bc1c4a38b0
towasme <towasm@gmail.com>
2020-05-17 21:41:53 -0500

    web dynamic now working

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -33,27 +33,24 @@ $.ajax({
   success: function (response) {
     for(let i = 0; i < response.length; i++) {
       let place = response[i];
-      $('.places').append('<article><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div>',
-      + '<div class="information"><div class="max_guest">' + place.max_guest + 'Guests' + place.max_guest + '</div><div class="number_rooms">' + place.number_rooms + 'Bedroom' + place.number_rooms + 's' + '</div>',
-      + '<div class="number_bathrooms">' + place.number_bathrooms + 'Bathroom' + place.number_bathrooms + 's' + '</div></div></div></article>')};
+      $('.places').append(`<article>
+      <div class="title_box">
+      <h2>${place.name}</h2>
+      <div class="price_by_night">${place.price_by_night}</div>   
+      </div>
+      <div class="information">
+      <div class="max_guest">${place.max_guest} Guest${place.max_guest != 1 ? 's' : ''}</div>                  
+      <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms != 1 ? 's' : ''}</div>                  
+      <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms != 1 ? 's' : ''}</div>  
+      </div>
+      </article>`)};
   }
 });
 
-
-
-/* $.ajax({
-  url: 'http://0.0.0.0:5001/api/v1/places_search',
-  type: 'POST',
-  data: '{}',
-  ContentType: 'application/json',
-  dataType: 'json',
-  success: function (data) {
-    for(let i = 0; i < data.length; i++) {
-      let place = data[i];
-      $('.places').append('<article><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div>',
-      + '<div class="information"><div class="max_guest">' + place.max_guest + 'Guests' + place.max_guest + '</div><div class="number_rooms">' + place.number_rooms + 'Bedroom' + place.number_rooms + 's' + '</div>',
-      + '<div class="number_bathrooms">' + place.number_bathrooms + 'Bathroom' + place.number_bathrooms + 's' + '</div></div>',
-      + '<div class="user"><b>Owner:</b>' + place.user.first_name + place.user.last_name + '</div><div class="description">' + place.description + '</div></article>')};
-  }
-});
-*/
+/* {
+      let place = response[i];
+      $('.places').append('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div></div>',
+      + '<div class="information"><div class="max_guest">' + place.max_guest + 'Guest' + {place.max_guest != 1 ? 's' : ''} + '</div><div class="number_rooms">',
+      + place.number_rooms + 'Bedroom' + {place.number_rooms != 1 ? 's' : ''} + '</div>',
+      + '<div class="number_bathrooms">' + place.number_bathrooms + 'Bathroom' + {place.number_bathrooms != 1 ? 's' : ''} + '</div></div></div></article>')};
+      */
