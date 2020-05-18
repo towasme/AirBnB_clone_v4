ae9c034bcf2ce0e23a0da8ca8a09b533c146bd82
towasme <towasm@gmail.com>
2020-05-18 15:32:23 -0500

    added extra =

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -26,24 +26,25 @@ $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
 });
 
 $.ajax({
-  type: "POST",
-  url: "http://0.0.0.0:5001/api/v1/places_search",
-  data: "{}",
+  type: 'POST',
+  url: 'http://0.0.0.0:5001/api/v1/places_search',
+  data: '{}',
   contentType: 'application/json',
   success: function (response) {
-    for(let i = 0; i < response.length; i++) {
-      let place = response[i];
+    for (let i = 0; i < response.length; i++) {
+      const place = response[i];
       $('.places').append(`<article>
       <div class="title_box">
       <h2>${place.name}</h2>
       <div class="price_by_night">${place.price_by_night}</div>   
       </div>
       <div class="information">
-      <div class="max_guest">${place.max_guest} Guest${place.max_guest != 1 ? 's' : ''}</div>                  
-      <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms != 1 ? 's' : ''}</div>                  
-      <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms != 1 ? 's' : ''}</div>  
+      <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>                  
+      <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>                  
+      <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>  
       </div>
-      </article>`)};
+      </article>`);
+    }
   }
 });
 
--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -12,32 +12,33 @@ $(document).ready(function () {
     } else {
       $('div.selection_amenities h4').html('&nbsp;');
     }
-  })
+  });
   $('.filters > button').click(function () {
     $('.places > article').remove();
     $.ajax({
       type: 'POST',
       url: 'http://0.0.0.0:5001/api/v1/places_search',
-      data: JSON.stringify({'amenities': Object.keys(amenitiesName)}),
+      data: JSON.stringify({ amenities: Object.keys(amenitiesName) }),
       dataType: 'json',
       contentType: 'application/json',
       success: function (data) {
         for (let i = 0; i < data.length; i++) {
-          let place = data[i];
+          const place = data[i];
           $('.places').append(`<article>
         <div class="title_box">
         <h2>${place.name}</h2>
         <div class="price_by_night">${place.price_by_night}</div>   
         </div>
         <div class="information">
-        <div class="max_guest">${place.max_guest} Guest${place.max_guest != 1 ? 's' : ''}</div>                  
-        <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms != 1 ? 's' : ''}</div>                  
-        <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms != 1 ? 's' : ''}</div>  
+        <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>                  
+        <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>                  
+        <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>  
         </div>
-        </article>`)}
+        </article>`);
+        }
       }
-    })
-  })
+    });
+  });
 });
 
 $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
@@ -51,24 +52,24 @@ $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
 });
 
 $.ajax({
-  type: "POST",
-  url: "http://0.0.0.0:5001/api/v1/places_search",
-  data: "{}",
+  type: 'POST',
+  url: 'http://0.0.0.0:5001/api/v1/places_search',
+  data: '{}',
   contentType: 'application/json',
   success: function (response) {
-    for(let i = 0; i < response.length; i++) {
-      let place = response[i];
+    for (let i = 0; i < response.length; i++) {
+      const place = response[i];
       $('.places').append(`<article>
       <div class="title_box">
       <h2>${place.name}</h2>
       <div class="price_by_night">${place.price_by_night}</div>   
       </div>
       <div class="information">
-      <div class="max_guest">${place.max_guest} Guest${place.max_guest != 1 ? 's' : ''}</div>                  
-      <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms != 1 ? 's' : ''}</div>                  
-      <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms != 1 ? 's' : ''}</div>  
+      <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>                  
+      <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>                  
+      <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>  
       </div>
-      </article>`)};
+      </article>`);
+    }
   }
 });
-
