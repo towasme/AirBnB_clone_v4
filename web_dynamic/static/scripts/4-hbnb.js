2687a822d9c3e26b94c1c56fd934b1c24a7920b0
paisap <santiagoaldana58@gmail.com>
2020-05-18 19:31:08 +0000

    task 5

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+  const amenitiesName = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      amenitiesName[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenitiesName[$(this).data('id')];
+    }
+    const lenList = Object.values(amenitiesName).length;
+    if (lenList > 0) {
+      $('div.selection_amenities h4').text(Object.values(amenitiesName).join(', '));
+    } else {
+      $('div.selection_amenities h4').html('&nbsp;');
+    }
+  });
+});
+
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+  if (status === 'success') {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  }
+});
+
+$.ajax({
+  type: "POST",
+  url: "http://0.0.0.0:5001/api/v1/places_search",
+  data: "{}",
+  contentType: 'application/json',
+  success: function (response) {
+    for(let i = 0; i < response.length; i++) {
+      let place = response[i];
+      $('.places').append(`<article>
+      <div class="title_box">
+      <h2>${place.name}</h2>
+      <div class="price_by_night">${place.price_by_night}</div>   
+      </div>
+      <div class="information">
+      <div class="max_guest">${place.max_guest} Guest${place.max_guest != 1 ? 's' : ''}</div>                  
+      <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms != 1 ? 's' : ''}</div>                  
+      <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms != 1 ? 's' : ''}</div>  
+      </div>
+      </article>`)};
+  }
+});
+$('.filters > button').click(function () {
+  $('.places > article').remove();
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    data: JSON.stringify({'amenities': Object.keys(amenitiesName)}),
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      for (let i = 0; i < data.length; i++) {
+        let place = data[i];
+        $('.places').append(`<article>
+      <div class="title_box">
+      <h2>${place.name}</h2>
+      <div class="price_by_night">${place.price_by_night}</div>   
+      </div>
+      <div class="information">
+      <div class="max_guest">${place.max_guest} Guest${place.max_guest != 1 ? 's' : ''}</div>                  
+      <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms != 1 ? 's' : ''}</div>                  
+      <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms != 1 ? 's' : ''}</div>  
+      </div>
+      </article>`)}
+    }
+  })
+});
